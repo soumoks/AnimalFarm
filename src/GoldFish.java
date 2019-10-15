a409d36bbd421ea5eae4df5b1b9e853a187849e2
soumoks <soumoks@gmail.com>
2019-10-15 11:08:53 -0600

    Added a GoldFish Class

---
+++ src/GoldFish.java
@@ -0,0 +1,9 @@
+public class GoldFish extends Animal{
+    public GoldFish(String name){
+        super(name);
+    }
+
+    public void makeSound(){
+        System.out.println("No Sound!");
+    }
+}
